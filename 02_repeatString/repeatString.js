c47b14c35de51f9ce90161c5e452db8055346027
Jakob <codemonkey727@gmail.com>
2024-08-23 14:04:38 -0400

    complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    let repeatedString = '';
 
+    if(number < 0){
+        return "ERROR";
+    }
+
+    for(let i = 0; i < number; i++){
+        repeatedString += word;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line

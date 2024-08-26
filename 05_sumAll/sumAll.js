2c9109be08c37d8794e6ee20ab2372c784e4ac14
Jakob <codemonkey727@gmail.com>
2024-08-26 18:31:39 -0400

    complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    let min = 0, max = 0, total = 0;
+
+    if(val1 < 0 || val2 < 0 || !Number.isInteger(val1) || !Number.isInteger(val2) ){
+        return 'ERROR';
+    }
+
+    if(val1 >= val2){
+        min = val2;
+        max = val1;
+    } else {
+        min = val1;
+        max = val2;
+    }
+
+    while(max >= min){
+        total += max;
+        max--;
+    }
+
+    return total;
 
 };
 

3e22c4ef710b5efd3144f3eae68b9a88faeaea82
Jakob <codemonkey727@gmail.com>
2024-09-17 20:13:10 -0400

    complete javascript exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.replace(/[\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase(); //uses regular expression to get rid of any punctuation therefore ignoring it during the comparison
+    let reversedString = newString.split('').reverse().join('');
 
+    if(newString === reversedString){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

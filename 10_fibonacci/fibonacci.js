38165300ec8a59ac7e3bcb5a6d1df6a6a8f69bf4
Jakob <codemonkey727@gmail.com>
2024-09-17 20:25:35 -0400

    complete javascript exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(limit) {
+    if(limit < 0){
+        return 'OOPS'
+    }
 
+    if(limit == 0){
+        return 0;
+    }
+
+    if(limit == 1){
+        return 1;
+    }
+
+    return fibonacci(limit - 1) + fibonacci(limit - 2);
 };
 
 // Do not edit below this line
